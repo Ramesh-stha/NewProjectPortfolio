2f071f8b88eaaf9c0c40b837fe4be35bd7f61659
puzan <rameshshrestha514@gmail.com>
2025-11-03 20:39:35 +0545

    adding contact jsx

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,11 +1,115 @@
-import React from 'react'
-
+import React from "react";
+import { useRef, useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import emailjs from "@emailjs/browser";
 const Contact = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const form = useRef();
+  const [isSent, setIsSent] = useState(false);
+  const sendEmail = (e) => {
+    e.preventDefault(); // preventing for auto refresh when submiting the form
 
-export default Contact
+    emailjs
+      .sendForm(
+        "service_gpvt3lo", // service id of emailjs
+        "template_fbkxcor", // template id of emailjs
+        form.current,
+        "koFY7HG4RIgKsIUlH" // public key of emailjs
+      )
+      .then(
+        () => {
+          setIsSent(true);
+          form.current.reset(); // to reset the form field after submiting
+          toast.success("Message sent successfully!", {
+            position: "top-right",
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "dark",
+          });
+        },
+        (error) => {
+          toast.error("Error sending message.", error);
+          toast.error("Failed to send the message, please try again.", {
+            position: "top-right",
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "dark",
+          });
+        }
+      );
+  };
+  return (
+    <section
+      id="contact"
+      className="flex flex-col items-center justify-center py-24 px-[12vw] md:px-[7vw] lg:px-[20vw] font-sans "
+    >
+      {/* Toast Container for notifications */}
+      <ToastContainer />
+
+      {/* Section Title */}
+      <div className="text-center mb-16">
+        <h2 className="text-4xl font-bold text-white">CONTACT</h2>
+        <div className="w-32 h-1 bg-purple-500 mx-auto mt-4"></div>
+        <p className="text-gray-400 mt-4 text-lg font-semibold">
+          Feel free to reach out for collaborations or just a friendly hello!
+        </p>
+      </div>
+      {/* Contact Information */}
+      <div className="mt-8 w-full max-w-md bg-[#0d081f] rounded-lg p-6 shadow-lg border border-gray-700">
+        <h3 className="text-xl font-semibold text-white text-center">
+          Connect With Me
+        </h3>
+        <form
+          ref={form}
+          onSubmit={sendEmail}
+          className="mt-4 flex flex-col space-y-4"
+        >
+          <input
+            type="email"
+            name="user_email"
+            placeholder="Your Email"
+            className="w-full p-3 rounded-md bg-[#131025] text-white border border-gray-600 focus:outline-none focus:border-purple-500 "
+            required
+          />
+          <input
+            type="text"
+            name="user_name"
+            placeholder="Your Name"
+            className="w-full p-3 rounded-md bg-[#131025] text-white border border-gray-600 focus:outline-none focus:border-purple-500 "
+            required
+          />
+          <input
+            type="text"
+            name="user_subject"
+            placeholder="Subject"
+            className="w-full p-3 rounded-md bg-[#131025] text-white border border-gray-600 focus:outline-none focus:border-purple-500 "
+            required
+          />
+          <textarea
+            name="message"
+            placeholder="Message"
+            rows="4"
+            className="w-full p-3 rounded-md bg-[#131025] text-white border border-gray-600 focus:outline-none focus:border-purple-500 "
+          ></textarea>
+          {/* Submit Button */}
+          <button
+            type="submit"
+            className="w-full bg-gradient-to-r from-purple-600 to-pink-500 py-5 text-white font-semibold rounded-md hover:opacity-90 transition"
+          >
+            Send
+          </button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
